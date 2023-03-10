73d5281205bddc702f385132a6dcc42405364442
rtg <rtg.chem@gmail.com>
2023-03-09 18:04:13 -0700

    Comments

--- fftItter.m
+++ fftItter.m
@@ -1,6 +1,11 @@
 function [dataOut] = fftItter(dataIn, lowNotch, highNotch, numbIter)
 
 %first want to see if the data loaded in is a peg file or just a csv
+%its going to be better to take in a structure always I think
+%also need to get the data from the user
+    %frequencey
+%need some logical tests to determine if the data is 2 dimensional or just
+%contains a single dimension (could accept FID or VUV data).
 
 %next want to put the data into FFTDenoise
 
