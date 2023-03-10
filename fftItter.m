75b7a48ace6de775354df3118e7f923615198a17
rtg <rtg.chem@gmail.com>
2023-03-09 17:29:58 -0700

    Added function fftItter

---
+++ fftItter.m
@@ -0,0 +1,13 @@
+function [dataOut] = fftItter(dataIn, lowNotch, highNotch, numbIter)
+
+%first want to see if the data loaded in is a peg file or just a csv
+
+%next want to put the data into FFTDenoise
+
+%next want to loop over the FFTDenoise function as many times as requested
+%by numbIter
+
+%finally want to output the data, I dont see any point of outputting plots
+%for the time being.
+
+end
